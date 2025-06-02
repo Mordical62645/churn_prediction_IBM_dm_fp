f3646561dbea07cbfd42c18e86acde28671ce701
Mordical62645 <p.tecson.rizal@gmail.com>
2025-06-02 13:28:13 +0800

    libraries imported; readme updated

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 #### LIBRARIES ####
+import pandas as pd
+import numpy as np
 
+# For splitting and scaling
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# For modeling and evaluation
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+# For visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# For handling imbalanced datasets
+from imblearn.over_sampling import SMOTE
+
+# For high-performance models
+import xgboost as xgb
+
+# Optional: for building a simple interactive web app
+import streamlit as st
